48fe84e716d199575334125534b0eb5cf68784be
maimai <maimai77@agileware.jp>
2015-01-15 19:09:40 +0900

    implement base

--- lib/crypt_keeper_with_ransack.rb
+++ lib/crypt_keeper_with_ransack.rb
@@ -3,4 +3,34 @@ require "crypt_keeper"
 require "ransack"
 
 module CryptKeeperWithRansack
+  module CryptKeeper
+    extend ActiveSupport::Concern
+
+    included do
+      alias_method_chain :crypt_keeper, :ransack
+    end
+
+    def crypt_keeper_with_ransack(*args)
+      crypt_keeper_without_ransack(*args)
+
+      crypt_keeper_fields.each do |field|
+        add_crypt_ransacker field, key: crypt_keeper_options[:key]
+      end
+    end
+  end
+
+  module Ransack
+    extend ActiveSupport::Concern
+
+    module ClassMethods
+      def add_crypt_ransacker(field, key: nil)
+        ransacker field do |parent|
+          Arel.sql "pgp_sym_decrypt(cast(#{field} AS bytea), '#{key}')"
+        end
+      end
+    end
+  end
 end
+
+CryptKeeper::Model::ClassMethods.include CryptKeeperWithRansack::CryptKeeper
+ActiveRecord::Base.include CryptKeeperWithRansack::Ransack
--- spec/crypt_keeper_with_ransack_spec.rb
+++ spec/crypt_keeper_with_ransack_spec.rb
@@ -5,7 +5,18 @@ describe CryptKeeperWithRansack do
     expect(CryptKeeperWithRansack::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    skip { expect(false).to eq(true) }
+  describe "#search" do
+    before :each do
+      User.create(name: 'MyText')
+    end
+
+    it { expect(User.find_by(name: 'MyText')).to be_nil }
+    it { expect(User.search_by_plaintext(:name, 'MyText').count).to eq 1 }
+
+    it { expect(User.where(name: 'MyText').count).to eq 0 }
+    it { expect(User.search(name_eq: 'MyText').result.count).to eq 1 }
+
+    it { expect(User.search(name_cont: 'My').result.count).to eq 1 }
+    it { expect(User.search(name_cont: 'Your').result.count).to eq 0 }
   end
 end
